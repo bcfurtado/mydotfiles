c74b466b65d2c88e7c977e10064735afcc0db7db
Bruno Furtado <brunofurtadoc@gmail.com>
2026-03-28 15:17:11 +0100

    feat: backup existing files during installation

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
+BACKUP_DIR=~/dotfiles-backup/$(date +%Y%m%d%H%M%S)
+
+link() {
+  local src=$(realpath "$1")
+  local dst="$2"
+  if [ -e "$dst" ] || [ -L "$dst" ]; then
+    mkdir -p "$BACKUP_DIR"
+    mv "$dst" "$BACKUP_DIR/"
+    echo "Backed up $dst to $BACKUP_DIR/"
+  fi
+  ln -s "$src" "$dst"
+}
+
 # Create symbolic links
-ln -s $(realpath ./mydotfiles/.gitconfig) ~/.gitconfig
-ln -s $(realpath ./mydotfiles/.gitconfig-personal) ~/.gitconfig-personal
-ln -s $(realpath ./mydotfiles/vim/.vimrc) ~/.vimrc
+link ./.gitconfig ~/.gitconfig
+link ./.gitconfig-personal ~/.gitconfig-personal
+link ./vim/.vimrc ~/.vimrc
+link ./.zshrc ~/.zshrc
+link ./.zshrc ~/.zlogin
