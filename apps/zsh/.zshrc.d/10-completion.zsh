f03a4b335445d3c7efc5e430b942c79d01a93d5d
Bruno Furtado <brunofurtadoc@gmail.com>
2026-04-06 01:15:49 +0200

    fix: use posix-compatible ps parameters

--- apps/zsh/.zshrc.d/10-completion.zsh
+++ apps/zsh/.zshrc.d/10-completion.zsh
@@ -2,6 +2,6 @@
 zstyle ':completion:*:*:make:*' tag-order targets variables files
 
 # kill: do not wrap long process and preview it at the bottom of the screen
-zstyle ':fzf-tab:complete:kill:argument-rest' fzf-preview 'ps --pid=$word -o cmd --no-headers -w -w'
+zstyle ':fzf-tab:complete:kill:argument-rest' fzf-preview 'ps -p $word -o command= -ww'
 zstyle ':fzf-tab:complete:kill:argument-rest' fzf-flags '--preview-window=down:3:wrap'
 zstyle ':fzf-tab:complete:kill:*' popup-pad 0 3
