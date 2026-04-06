b2afcf831cdc8998da6ca7e5f4e76b7a855369b6
Bruno Furtado <brunofurtadoc@gmail.com>
2026-04-06 11:58:45 +0200

    feat: add linux support

--- install.sh
+++ install.sh
@@ -6,16 +6,35 @@ NC='\033[0m'
 
 info() { echo -e "${GREEN}$1${NC}"; }
 
-# Install brew packages
-info "Installing brew packages..."
-brew bundle install --file=Brewfile
+detect_os() {
+  case "$(uname -s)" in
+    Darwin) echo "macos" ;;
+    Linux)  echo "linux" ;;
+    *)      echo "unknown" ;;
+  esac
+}
+
+OS=$(detect_os)
+
+# Install packages
+if [ "$OS" = "macos" ]; then
+  info "Installing brew packages..."
+  brew bundle install --file=Brewfile
+elif [ "$OS" = "linux" ]; then
+  info "Installing Linux packages..."
+  ./apps-linux/install-linux-packages.sh
+else
+  echo "Unsupported OS: $(uname -s)" && exit 1
+fi
 
 # Create symlinks and extra files
 info "Creating symlinks and extra files..."
 ./apps/install-dotfiles.sh
 
 # Configure macOS apps and preferences
-info "Configuring macOS apps..."
-./apps-macos/install-macos-apps.sh
+if [ "$OS" = "macos" ]; then
+  info "Configuring macOS apps..."
+  ./apps-macos/install-macos-apps.sh
+fi
 
 info "Done"
