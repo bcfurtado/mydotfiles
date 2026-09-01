a1677d65c86057bed9ada26adfefa6391403d3c0
Bruno Furtado <brunofurtadoc@gmail.com>
2026-09-02 01:08:12 +0200

    feat: add docker aliases

--- apps/zsh/.zshrc.d/30-aliases.zsh
+++ apps/zsh/.zshrc.d/30-aliases.zsh
@@ -4,3 +4,8 @@ alias k="kubectl"
 alias ll="ls -alFh"
 alias openports="lsof -iTCP -sTCP:LISTEN -P -n | ov --column-rainbow --column-width --header 1"
 alias tf="terraform"
+alias dps="docker ps"
+
+if ! command -v docker >/dev/null 2>&1; then
+  alias lazydocker='DOCKER_HOST="unix://$(podman machine inspect --format "{{.ConnectionInfo.PodmanSocket.Path}}")" command lazydocker'
+fi
